a6025fb57e191a84e4019236e10b3404c6095080
msra800 <v-zeyuanlai@microsoft.com>
2025-09-03 06:27:54 -0700

    fix encoder

--- encode_latent.py
+++ encode_latent.py
@@ -22,7 +22,17 @@ def setup_audioldm2_vae(gpu_id, repo_id="cvssp/audioldm2", torch_dtype=torch.flo
     pipe = pipe.to(device)
     print(f"[GPU-{gpu_id}]: 模型已成功加载。")
     return pipe.vae, pipe.feature_extractor, device
-
+def pad_wav(waveform, segment_length):
+    waveform_length = waveform.shape[-1]
+    assert waveform_length > 100, "Waveform is too short, %s" % waveform_length
+    if segment_length is None or waveform_length == segment_length:
+        return waveform
+    elif waveform_length > segment_length:
+        return waveform[:segment_length]
+    elif waveform_length < segment_length:
+        temp_wav = np.zeros((1, segment_length))
+        temp_wav[:, :waveform_length] = waveform
+    return temp_wav
 
 def _pad_spec(fbank, target_length=1024):
     n_frames = fbank.shape[0]
