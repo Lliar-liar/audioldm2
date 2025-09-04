2765befd7e71c9a5fb8275e94f281a0f25da3ac2
msra800 <v-zeyuanlai@microsoft.com>
2025-09-04 07:04:08 -0700

    9.4

--- encode_latent_ddp.py
+++ encode_latent_ddp.py
@@ -328,8 +328,8 @@ if __name__ == '__main__':
     # video_directory_list=["vggsound_04_3s"]
     # video_directory_list = ["vggsound_00_3s","vggsound_01_3s","vggsound_02_3s","vggsound_03_3s","vggsound_04_3s", 
     # "vggsound_06_3s", "vggsound_07_3s", "vggsound_08_3s", "vggsound_09_3s",]
-    # video_directory_list = ["vggsound_10_3s", "vggsound_11_3s", "vggsound_12_3s", "vggsound_13_3s", "vggsound_14_3s",]
-    video_directory_list =["vggsound_15_3s", "vggsound_16_3s", "vggsound_17_3s", "vggsound_18_3s", "vggsound_19_3s",]
+    video_directory_list = ["vggsound_10_3s", "vggsound_11_3s", "vggsound_12_3s", "vggsound_13_3s", "vggsound_14_3s",]
+    # video_directory_list =["vggsound_15_3s", "vggsound_16_3s", "vggsound_17_3s", "vggsound_18_3s", "vggsound_19_3s",]
     #     "vggsound_15_3s", "vggsound_16_3s", "vggsound_17_3s", "vggsound_18_3s", "vggsound_19_3s",
     # ]
     input_video_directory_base="/blob/vggsound_cropped"
